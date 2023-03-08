460f8ea0349b1accf83f821762417068f7c8c1a2
coder-sam-007 <sameerjain.coder@gmail.com>
2023-03-08 12:38:22 +0530

    it's working

---
+++ client/script.js
@@ -0,0 +1,59 @@
+const userText = document.querySelector("#user-text");
+const response_text = document.querySelector("#response-text");
+const startBTN = document.querySelector("#start");
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.interimResults = false;
+
+const getAnswear = async (question) => {
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: question,
+    }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    return parsedData;
+  } else {
+    const err = await response.text();
+    return `something wrong happened.. \nthe error is ${err}`;
+  }
+};
+
+startBTN.addEventListener("click", () => {
+  recognition.start();
+});
+
+recognition.onstart = () => {
+  console.log("Voice recognition started. Speak now!");
+};
+
+recognition.onresult = async (event) => {
+  const speechToText = event.results[0][0].transcript;
+  userText.textContent = speechToText;
+  let question = userText.textContent;
+
+  const answer = await getAnswear(question);
+  response_text.textContent = answer;
+
+  const speech = new SpeechSynthesisUtterance(answer);
+  window.speechSynthesis.speak(speech);
+};
+
+recognition.addEventListener("error", (event) => {
+  console.log(`Recognition error: ${event.error}`);
+});
+
+recognition.addEventListener("end", () => {
+  console.log("Speech recognition ended");
+});
