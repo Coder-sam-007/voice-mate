a51f6246736c2bf40bdbcbb1a7992a6b74d9be67
coder-sam-007 <sameerjain.coder@gmail.com>
2023-03-08 17:48:48 +0530

    cors changed

--- server/server.js
+++ server/server.js
@@ -13,6 +13,14 @@ const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(
+  (req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  },
   cors({
     origin: "*",
     methods: "GET,POST,OPTIONS",
